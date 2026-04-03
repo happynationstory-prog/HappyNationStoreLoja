3361326c1663b7e9b23e8973ba803a6de716d15d
happynationstory-prog <happynationstory@gmail.com>
2026-04-03 20:17:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+`base: '/HappyNationStoreLoja/'
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
